f805c7acf85c3359ba77f05296810d736ee2e4d3
Ruslan Vanzhula <r.vanzhula@gmail.com>
2026-02-21 17:13:51 +0200

    exports *

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-export type { Contract, Handler } from "./lib/cqrs.types.ts";
-
-export { DIContext } from "./lib/di-context.js";
-
-export type {
-	AnyModule,
-	ExtractModuleDef,
-	MandatoryNameAndRegistrationPair,
-	Module,
-	ModuleDef,
-	StaticModule,
-} from "./lib/di-context.types.js";
-export { createFactoryProvider } from "./lib/di-context.types.js";
+export type * from "./lib/cqrs.types.ts";
+export * from "./lib/di-context.js";
+export * from "./lib/di-context.types.js";
