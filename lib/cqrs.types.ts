acb93f1b3a0474b79653c7fb0c9adb9a38d8f08a
Ruslan Vanzhula <r.vanzhula@gmail.com>
2026-02-21 17:27:32 +0200

    made Handler more generic

--- lib/cqrs.types.ts
+++ lib/cqrs.types.ts
@@ -7,7 +7,7 @@ export type Contract<K extends PropertyKey, P, R> = {
 
 type Meta = Record<string, unknown>;
 
-type AnyContract = Contract<string, any, any>;
+type AnyContract = Contract<string, unknown, unknown>;
 
 export interface Handler<C extends AnyContract, K extends keyof C = keyof C> {
 	readonly key: K;
